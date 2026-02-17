9b7128ba3c2160628115ec9ef7b80cca6605a95a
LucasA1206 <lucas.ableson@student.uts.edu.au>
2026-02-17 12:05:51 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverActions: {
-    bodySizeLimit: '100mb',
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '100mb',
